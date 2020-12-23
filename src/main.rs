2802f2767a84fb653583399cb31cf928617e1bff
Isaac Clayton <slightknack@gmail.com>
2020-12-23 18:02:48 +0100

    got ecc key-pair generation and shared secret exchange working

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,24 @@ pub mod ui;
 pub mod cli;
 
 pub fn main() {
-    network::public::KeyPair::generate();
+    println!("\nSmall ecc key-pair test");
+    println!("-----------------------");
+
+    let my_key   = network::key::KeyPair::generate();
+    let your_key = network::key::KeyPair::generate();
+
+    println!("my public key:   {}", network::key::hex(my_key.public_bytes()));
+    println!("your public key: {}", network::key::hex(your_key.public_bytes()));
+    println!("-----------------------");
+
+    let my_side_shared   = my_key.shared_secret(&your_key.public_bytes());
+    let your_side_shared = your_key.shared_secret(&my_key.public_bytes());
+
+    println!("my side shared secret:   {}", network::key::hex(my_side_shared.unwrap().to_vec()));
+    println!("your side shared secret: {}", network::key::hex(your_side_shared.unwrap().to_vec()));
+    println!("-----------------------");
+
+    println!("test passed: {}\n", my_side_shared == your_side_shared);
+
     // ui::run();
 }
--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,1 +1,1 @@
-pub mod public;
+pub mod key;
